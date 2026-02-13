b6ae2fc05ef229354b29f8841f59a682ca927beb
MaRiddleH <ma_ruihao@163.com>
2026-02-13 16:32:02 +0800

    增加翻译功能

--- run_mineru_convert.bat
+++ run_mineru_convert.bat
@@ -17,8 +17,22 @@ rem 5. Run mineru command
 echo Running mineru command...
 mineru -p ./ -o ./
 
-rem 6. Convert Markdown files to DOCX
+cd ../
+
+
 echo Converting Markdown files to DOCX...
-python ../convert_md_to_docx.py
+python ./convert_md_to_docx.py
+
+rem 6. Move all files from aims to Output folder
+echo Moving files to output folder...
+if not exist output mkdir output
+xcopy /s /e /y aims\* output\
+echo Files moved successfully to output folder
+
+rem 7. Delete all files in aims folder
+echo Deleting all files in aims folder...
+for /d %%i in (aims\*) do rd /s /q "%%i"
+del /q aims\*.*
+echo All files in aims folder deleted successfully
 
 pause
